ce12be53d735662b63706ed8c33063c467b7bb45
adrimartin6 <adrianmartincalvet@gmail.com>
2024-11-08 09:19:28 +0100

    index.php actualizado

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-// session_start();
+session_start();
 
-// if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-//     header("Location: ./public/dashboard.php");
-//     exit();
-// } else {
-//     header("Location: ./public/login.php");
-//     exit();
-// }
-?>
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
+    header("Location: ./public/dashboard.php");
+    exit();
+} else {
+    header("Location: ./public/login.php");
+    exit();
+}
+
