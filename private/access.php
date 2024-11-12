4a0987093b2444730fe1b2ab12d374ce8b17a6f4
adrimartin6 <adrianmartincalvet@gmail.com>
2024-11-12 15:59:57 +0100

    Archivo access actualizado

--- private/access.php
+++ private/access.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         }
 
-        mysqli_stmt_close($stmt);
+        // mysqli_stmt_close($stmt);
     } else {
         $_SESSION['error'] = "Error en la consulta: " . mysqli_error($conn);
         header("Location: ../index.php");
